ab5290baf870fd003292d5a7b5e499c386c2d2cb
fullStackZac <zac.childress.sr@gmail.com>
2022-10-12 15:34:38 -0500

    geting play surface ready

--- game.py
+++ game.py
@@ -4,12 +4,18 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Chloe & The Lantern Empire')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('Red')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    #draw all elements
-    #update everything
+    
+    screen.blit(test_surface, (200,100))
+
     pygame.display.update()
+    clock.tick(60)
