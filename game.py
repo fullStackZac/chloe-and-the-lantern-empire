8dc6ba086b697ecbe723fc1068c88113fa3275c5
fullStackZac <zac.childress.sr@gmail.com>
2022-10-15 04:58:45 -0500

    beginning work on core

--- game.py
+++ game.py
@@ -2,12 +2,17 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Chloe & The Lantern Empire')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Red')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('New Font', False, 'Black')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
 
 while True:
     for event in pygame.event.get():
@@ -15,7 +20,12 @@ while True:
             pygame.quit()
             exit()
     
-    screen.blit(test_surface, (200,100))
-
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface,(300,50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100: snail_x_pos = 800
+    screen.blit(snail_surface,(snail_x_pos,250))
+   
     pygame.display.update()
     clock.tick(60)
